9f66c511e277938ac833a3a87ce7fef6148490cb
Obidkhandev <obidkhandev@gmail.com>
2024-03-13 05:10:38 +0500

    some change

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:practise/screens/my_items/app_bar.dart';
-import 'package:practise/screens/my_items/sliver_list.dart';
 
 import '../../my_sliver_delegate.dart';
 
@@ -16,14 +14,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: [
-          const MyAppBar(),
-          SliverPersistentHeader(pinned: true, delegate: CategoryItem()),
-          const MySliverList(),
-        ],
-      ),
+
     );
   }
 }
