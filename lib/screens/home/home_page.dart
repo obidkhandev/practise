cb4b11b16bc223baeb9c1285f65611c78e4128eb
Obidkhandev <obidkhandev@gmail.com>
2024-03-18 23:46:23 +0500

    some change

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:practise/screens/home/buttons.dart';
+import 'package:practise/screens/home/viewModel/calculate_view_model.dart';
 import 'package:practise/utils/size/size_utils.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -20,31 +22,34 @@ class HomePage extends StatelessWidget {
                 padding: const EdgeInsets.all(20),
                 alignment: Alignment.bottomRight,
                 width: double.infinity,
-                child: Text("Data"),
+                child: Consumer<CalculateViewModel>(
+                  builder: (context, viewModel, child) {
+                    return Text(
+                      "${viewModel.numberOne}${viewModel.operand}${viewModel.numberTwo}"
+                              .isEmpty
+                          ? '0'
+                          : "${viewModel.numberOne}${viewModel.operand}${viewModel.numberTwo}",
+                      style: TextStyle(color: Colors.black,fontSize: 24),
+                    );
+                  },
+                ),
               ),
             ),
             Expanded(
-                flex: 3,
-                child: Container(
-                    decoration: const BoxDecoration(
-                      color: Color(0xffE9F6FF),
-                    ),
-                    child: Wrap(
-                      children: Btn.buttonValues.map((e) {
-                        return SizedBox(
-                          width: MediaQuery
-                              .of(context)
-                              .size
-                              .width / 4, height: MediaQuery
-                            .of(context)
-                            .size
-                            .height / 9,
-                          child: buildButton(e),
-                        );
-                      }
-                      ).toList(),
-                    )
-                ),
+              flex: 3,
+              child: Container(
+                  decoration: const BoxDecoration(
+                    color: Color(0xffE9F6FF),
+                  ),
+                  child: Wrap(
+                    children: Btn.buttonValues.map((e) {
+                      return SizedBox(
+                        width: MediaQuery.of(context).size.width / 4,
+                        height: MediaQuery.of(context).size.height / 9,
+                        child: buildButton(e),
+                      );
+                    }).toList(),
+                  )),
             ),
           ],
         ),
@@ -53,29 +58,42 @@ class HomePage extends StatelessWidget {
   }
 
   Widget buildButton(value) {
-    return InkWell(
-      onTap: () {},
-      child: Center(
+    return Consumer<CalculateViewModel>(builder: (context, viewModel, child) {
+      return InkWell(
+        onTap: () {
+          debugPrint(value);
+          viewModel.onTapButton(value);
+          debugPrint(viewModel.operand);
+          debugPrint(viewModel.numberOne);
+          debugPrint(viewModel.numberTwo);
+        },
+        child: Center(
           child: Text(
             value,
-            style:  TextStyle(fontWeight: FontWeight.bold,fontSize: 32 ,color: getBtnColor(value)),
-          )),
-    );
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+              color: getBtnColor(value),
+            ),
+          ),
+        ),
+      );
+    });
   }
 
   Color getBtnColor(value) {
     return [Btn.clr, Btn.del].contains(value)
         ? Colors.red
         : [
-      Btn.per,
-      Btn.multpley,
-      Btn.add,
-      Btn.subtract,
-      Btn.divide,
-      Btn.calculate,
-      Btn.history,
-    ].contains(value)
-        ? Color(0xff00B0D7)
-        : Colors.black87;
+            Btn.per,
+            Btn.multpley,
+            Btn.add,
+            Btn.subtract,
+            Btn.divide,
+            Btn.calculate,
+            Btn.history,
+          ].contains(value)
+            ? Color(0xff00B0D7)
+            : Colors.black87;
   }
 }
--- lib/screens/home/viewModel/calculate_view_model.dart
+++ lib/screens/home/viewModel/calculate_view_model.dart
@@ -1,86 +1,99 @@
 import 'package:flutter/foundation.dart';
 import '../buttons.dart';
 
-class CalculateViewModel extends ChangeNotifier{
+class CalculateViewModel extends ChangeNotifier {
   String numberOne = '';
   String numberTwo = '';
   String operand = '';
 
-  void onTapButton(String btn){
-    if(btn == Btn.del){
+  void onTapButton(String btn) {
+    if (btn == Btn.del) {
       delete();
+      return;
     }
-    if(btn == Btn.per){
+    if (btn == Btn.per) {
       convertToPercent();
+      return;
     }
-    if(btn == Btn.clr){
+    if (btn == Btn.clr) {
       clearAll();
+      return;
     }
-    if(btn == Btn.calculate){
+    if (btn == Btn.calculate) {
       calculate();
+      return;
     }
     appendsValue(btn);
+    // return;
   }
 
-  void appendsValue(String value){
-    if(value != Btn.dot && int.tryParse(value) != null ){
-      if(operand.isNotEmpty && numberTwo.isNotEmpty){
+  void appendsValue(String value) {
+    if (value != Btn.dot && int.tryParse(value) != null) {
+      if (operand.isNotEmpty && numberTwo.isNotEmpty) {
         calculate();
       }
       operand = value;
-    }
-    else if(numberOne.isNotEmpty && operand.isNotEmpty){
-      if(value == Btn.dot && numberOne.contains(Btn.dot)) return;
-      if(value == Btn.dot && numberOne.isEmpty || value == Btn.n0){
+    } else if (numberOne.isNotEmpty && operand.isNotEmpty) {
+      if (value == Btn.dot && numberOne.contains(Btn.dot)) return;
+      if (value == Btn.dot && numberOne.isEmpty || value == Btn.n0) {
         value = '0.';
       }
+      numberOne += value;
+    } else if (numberTwo.isNotEmpty && operand.isNotEmpty) {
+      if (value == Btn.dot && numberTwo.contains(Btn.dot)) return;
+      if (value == Btn.dot && numberTwo.isEmpty || value == Btn.n0) {
+        value = '0.';
+      }
+      numberTwo += value;
     }
-    numberTwo += value;
+
     notifyListeners();
   }
 
-  void calculate(){
-    if(numberOne.isNotEmpty && numberTwo.isNotEmpty && operand.isNotEmpty) {
-      final double num1 = double.parse(numberOne);
-      final double num2 = double.parse(numberTwo);
-      var result = 0.0;
-      switch(operand){
-        case Btn.add:
-          result = num1+num2;
-          break;
-        case Btn.subtract:
-          result = num1-num2;
-          break;
-        case Btn.divide:
-          result = num1-num2;
-          break;
-        case Btn.multpley:
-          result = num1*num2;
-          break;
-        default:
-      }
-      numberOne = '$result';
-      if (numberOne.endsWith('.0')) {
-        numberOne = numberOne.substring(0, numberOne.length - 2);
-      }
-      operand = '';
-      numberTwo = '';
-      notifyListeners();
+  void calculate() {
+    if (numberOne.isEmpty) return;
+    if (numberTwo.isEmpty) return;
+    if (operand.isEmpty) return;
+
+    final double num1 = double.parse(numberOne);
+    final double num2 = double.parse(numberTwo);
+    var result = 0.0;
+    switch (operand) {
+      case Btn.add:
+        result = num1 + num2;
+        break;
+      case Btn.subtract:
+        result = num1 - num2;
+        break;
+      case Btn.divide:
+        result = num1 / num2;
+        break;
+      case Btn.multpley:
+        result = num1 * num2;
+        break;
+      default:
     }
+    numberOne = '$result';
+    if (numberOne.endsWith('.0')) {
+      numberOne = numberOne.substring(0, numberOne.length - 2);
+    }
+    operand = '';
+    numberTwo = '';
+    notifyListeners();
   }
 
-  void clearAll (){
+  void clearAll() {
     numberOne = '';
     numberTwo = '';
     operand = '';
     notifyListeners();
   }
 
-  void convertToPercent(){
-    if(numberOne.isNotEmpty && operand.isNotEmpty && numberTwo.isNotEmpty){
+  void convertToPercent() {
+    if (numberOne.isNotEmpty && operand.isNotEmpty && numberTwo.isNotEmpty) {
       calculate();
     }
-    if(operand.isNotEmpty){
+    if (operand.isNotEmpty) {
       return;
     }
     final number = double.parse(numberOne);
@@ -90,17 +103,14 @@ class CalculateViewModel extends ChangeNotifier{
     notifyListeners();
   }
 
-  void delete(){
-    if(numberTwo.isNotEmpty){
-      numberTwo = numberTwo.substring(0,numberOne.length - 1);
-    }else if(numberOne.isNotEmpty){
-      numberOne = numberOne.substring(0,numberTwo.length - 1);
-    }else if(operand.isNotEmpty){
+  void delete() {
+    if (numberTwo.isNotEmpty) {
+      numberTwo = numberTwo.substring(0, numberTwo.length - 1);
+    } else if (numberOne.isNotEmpty) {
+      numberOne = numberOne.substring(0, numberOne.length - 1);
+    } else if (operand.isNotEmpty) {
       operand = '';
     }
     notifyListeners();
   }
 }
-
-
-
